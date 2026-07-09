3abe27a8de01c8724aa4b4f4a85650e12a6279bb
tprjwrckst-art <tprjwrckst@privaterelay.appleid.com>
2026-07-09 23:20:42 +0100

    Add ESLint config for services/api

---
+++ services/api/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  rules: {}
+};
